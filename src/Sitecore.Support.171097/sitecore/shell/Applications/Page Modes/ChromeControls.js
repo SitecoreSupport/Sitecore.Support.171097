249b5eb7ad4d7ac806224045d8f867fa6e2bb49c
Adnan Faisal <afa@sitecore.net>
2018-10-19 11:05:40 -0400

    sitecore/shell/Applications/Page Modes/ChromeControls.js of the previous patch is updated to merge with the changes made in version 9.0 Update 2

--- src/Sitecore.Support.171097/sitecore/shell/Applications/Page Modes/ChromeControls.js
+++ src/Sitecore.Support.171097/sitecore/shell/Applications/Page Modes/ChromeControls.js
@@ -2,6 +2,7 @@
     constructor: function () {
         var cssClass = Sitecore.PageModes.PageEditor.languageCssClass() + (Sitecore.PageModes.Utility.isIE ? " ie" : "");
         this.toolbar = $sc("<div class='scChromeToolbar " + cssClass + "'></div>");
+
         // Sitecore.Support.171097
         this.renderings = {};
 
@@ -298,32 +299,45 @@
         return tag;
     },
 
+    setLoadingAnimation: function (element, show) {
+        element.children("img[id='myItemsLoadingIndicator']:first").remove();
+        if (show) {
+            var gif = '<img id="myItemsLoadingIndicator" src="/sitecore/shell/client/Speak/Assets/img/Speak/ProgressIndicator/sc-spinner16.gif" style="margin-left: 5px; height: 10px; width: 10px;">';
+            element.append(gif);
+        }
+    },
+
     renderDatasourceUsagesCommand: function () {
+        var self = this;
         var container = $sc("<a href='#' class='scChromeCommand scChromeMoreSection'></a>");
         var tag = $sc("<span class='scChromeCommandText'></span>");
+        tag.text(Sitecore.PageModes.Texts.Usage);
+        this.setLoadingAnimation(tag, true);
         var experienceEditor = Sitecore.PageModes.PageEditor.ExperienceEditor();
         var context = experienceEditor.generateDefaultContext();
         var itemId = this.getItemId();
         var database = context.currentContext.database;
         var version = this.getItemVersion();
+
+
+        // Sitcore.Support.171097
         context.currentContext.itemId = itemId;
         context.currentContext.value = this.usagesPathPatterns.join("|");
 
-        // Sitecore.Support.171097
+
         if (this.chrome._originalDOMElement.context.id in this.renderings && this.renderings[this.chrome._originalDOMElement.context.id] > 0) {
             tag.text(Sitecore.PageModes.Texts.Usage + " " + this.renderings[this.chrome._originalDOMElement.context.id]);
         }
         else {
-            var count;
             experienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.Datasources.GetDatasourceUsagesCount", function (response) {
                 var result = response.responseValue.value;
-                count = result ? result.length : 1;
+                var count = result ? result.length : 1;
                 tag.text(Sitecore.PageModes.Texts.Usage + " " + count);
+                self.setLoadingAnimation(tag, false);
                 container.attr("title", Sitecore.PageModes.Texts.TheNumberOfWebpagesIncludingThisOneWhereThisAssociatedContentIsUsed.replace("{0}", count));
-            }).execute(context);
+            }, undefined, true).execute(context);
             this.renderings[this.chrome._originalDOMElement.context.id] = count;
         }
-
         tag.click($sc.proxy(function (e) {
             e.stop();
 
@@ -650,8 +664,9 @@
             commandsRow.append(this.renderWorkflowSection());
         }
 
+
         if (!isDatasourceUsagesCommand) {
-            /* The "datasource usages" section */
+            // The "datasource usages" section 
             isDatasourceUsagesCommand = true;
             var datasourceUsagesCommand = this.renderDatasourceUsagesCommand();
             if (datasourceUsagesCommand) {
@@ -697,6 +712,7 @@
         if (this.chrome.data.contextItemUri == null) {
             return null;
         }
+
         var contextUriArray = this.chrome.data.contextItemUri.split("/");
         return contextUriArray[contextUriArray.length - 1].split("?")[0];
     },
@@ -724,6 +740,10 @@
         var experienceEditor = Sitecore.PageModes.PageEditor.ExperienceEditor();
         var context = experienceEditor.generateDefaultContext();
         context.currentContext.itemId = this.getItemId();
+        if (!context.currentContext.itemId) {
+            return null;
+        }
+
         var result = null;
         experienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.Workflow.GetWorkFlowCommands", function (response) {
             result = response.responseValue.value;
@@ -897,11 +917,11 @@
         this._overlay.show();
     }
 },
-{
-    commandRenderers: {},
-    eventNameSpace: "chromecontrols.",
-    registerCommandRenderer: function (commandName, renderer, chromeType) {
-        var key = chromeType != null ? chromeType.key() + ":" + commandName : commandName;
-        Sitecore.PageModes.ChromeControls.commandRenderers[key] = renderer;
-    }
-});
+    {
+        commandRenderers: {},
+        eventNameSpace: "chromecontrols.",
+        registerCommandRenderer: function (commandName, renderer, chromeType) {
+            var key = chromeType != null ? chromeType.key() + ":" + commandName : commandName;
+            Sitecore.PageModes.ChromeControls.commandRenderers[key] = renderer;
+        }
+    });
